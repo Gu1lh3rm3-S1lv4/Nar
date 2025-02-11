75a2c278ed393f91fbcf1d3babdaa455880a7707
Guilherme Silva <guih.bittencourt23@gmail.com>
2025-02-10 22:35:09 -0300

    Add files via upload

---
+++ nar.js
@@ -0,0 +1,9 @@
+ function Sim() {
+    alert('voçe aceitou namorar comigo :');
+
+}
+    function desvia(){
+        console.log ("opa, desviei");
+        var btn = document.querySelector('')
+
+    }
