a5b0269f7f7e83f4124b1c1c05a16474c920f93e
Guilherme Silva <guih.bittencourt23@gmail.com>
2025-05-12 12:38:13 -0300

    Delete nar.js

--- nar.js
+++
@@ -1,9 +0,0 @@
- function Sim() {
-    alert('voçe aceitou namorar comigo :');
-
-}
-    function desvia(){
-        console.log ("opa, desviei");
-        var btn = document.querySelector('')
-
-    }
