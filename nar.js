90090482f6b87fd96257977c9e12b81c30b5b1cd
Guilherme Silva <guih.bittencourt23@gmail.com>
2025-05-12 12:42:02 -0300

    Add files via upload

---
+++ nar.js
@@ -0,0 +1,15 @@
+function sim() {
+    alert('voçe aceitou namorar comigo !! 😛');
+
+}
+    function desvia(t){
+        var btn = t;
+        btn.style.position = 'absolute';
+        btn.style.bottom = geraPosicao(10, 90);
+        btn.style.left = geraPosicao(10, 90);
+        console.log('opa, desviei');
+}
+
+function geraPosicao(min, max) {
+  return (Math.random() * (max - min) + min) + '%';
+}
